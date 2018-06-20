509d8062a14c76d8f7b45fd67938d6be99faaddf
Gooner14 <38190318+Gooner14@users.noreply.github.com>
2018-06-20 22:48:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -54,11 +54,11 @@ def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
     city = parameters.get("geo-city")
-    day=parameters.get("date")
+    day=parameters.get("dat")
     if city is None:
         return None
 
-    return "select item.forecast from weather.forecast where (woeid in (select woeid from geo.places(1) where text='" + city + "') and item.forecast.date=' day ') limit 1"
+    return "select item.forecast from weather.forecast where (woeid in (select woeid from geo.places(1) where text='" + city + "') and item.forecast.day=' day ') limit 1"
 
 def makeWebhookResult(data):
     query = data.get('query')
