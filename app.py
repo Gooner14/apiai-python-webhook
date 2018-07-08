bb79cca7081d827591c79c1322604a4ef60fd3c2
Gooner14 <38190318+Gooner14@users.noreply.github.com>
2018-07-08 17:59:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -242,8 +242,8 @@ def makeWebhookResult2(data,city):
     aqi_json = json.loads(aqi_req.text)
     # print(json.dumps(item, indent=4))
 
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \ 
-    ",the temperature is " + condition.get('temp') + " " + units.get('temperature') + " aqi is "
+    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
     print("Response:")
     print(speech)
