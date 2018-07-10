59a075667f0b0e68a179540e1e4c4b400a48d592
Gooner14 <38190318+Gooner14@users.noreply.github.com>
2018-07-10 20:25:02 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -32,9 +32,9 @@ def webhook():
 
 def processRequest(req):
     print ("started processing")
-    if req.get("result").get("action") != "PollForecast":
+    if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
-    #baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
     result = req.get("result")
     parameters = result.get("parameters")
     #import ipgetter
@@ -45,51 +45,155 @@ def processRequest(req):
     #r = requests.get(url)
     #js = r.json()
     city = parameters.get("geo-city")
-    #day=parameters.get("dat")
-    """newlink = "https://api.ipdata.co/city?api-key=1ad57590c9de8df36fae6f8693b934d2ca8d6228e6f5f5ab8e7cc6b7"
-    newf = requests.get(newlink)
-    city=newf.text"""
-    """g = geocoder.ip('me')
-    print(g.city)
-    city=g.city"""
-    lati=22.242525
-    longi=77.45435
+    day=parameters.get("dat")
+    if len(city)<3:
+        """newlink = "https://api.ipdata.co/city?api-key=1ad57590c9de8df36fae6f8693b934d2ca8d6228e6f5f5ab8e7cc6b7"
+        newf = requests.get(newlink)
+        city=newf.text"""
+        """g = geocoder.ip('me')
+        print(g.city)
+        city=g.city"""
+        send_url = "http://api.ipstack.com/check?access_key=e4bc9f0507a494f428e34e9bdad24e95&format=1curl%20freegeoip.net/json"
+        geo_req = requests.get(send_url)
+        geo_json = json.loads(geo_req.text)
+        city = geo_json['city']
+        print (city)
+    #app.wsgi_app = ProxyFix(app.wsgi_app)    
+    if len(day) <3:
+        yql_query = makeYqlQuery2(req,city)
+    else:    
+        yql_query = makeYqlQuery1(req,city)
+    print ("yql query created")
+    if yql_query is None:
+        print("yqlquery is empty")
+        return {}
+    yql_url = baseurl    + urllib.urlencode({'q': yql_query}) + "&format=json"
+    print(yql_url)
 
-    #app.wsgi_app = ProxyFix(app.wsgi_app)       
-    lati=str(lati)
-    longi=str(longi)
-    aqi_url="http://api.breezometer.com/baqi/?lat="+lati+"&lon="+longi+"&key=e89840e7891c4a81a95e10156c1d9dcf"+"&format=json"
-    
-    print(aqi_url)
-    result = urllib.urlopen(aqi_url).read()
-    print("aqi result: ")
+    result = urllib.urlopen(yql_url).read()
+    print("yql result: ")
     print(result)
 
     data = json.loads(result)
-    res = makeWebhookResult2(data)
-    #res = makeWebhookResult1(data,city)
+    if len(day) < 3:
+        res = makeWebhookResult2(data,city)
+    else:
+        res = makeWebhookResult1(data,city)
     return res
+
+
+def makeYqlQuery1(req,city):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    #city = parameters.get("geo-city")
+    print("in makeyqlquery")
+    print(city)
+    day=parameters.get("dat")
+    if city is None:
+        return None    
+    #return "select * from weather.forecast where (woeid in (select woeid from geo.places(1) where text='" + city + "') and item.forecast.day='" + day + "') limit 1"
+    return "select units,location,title, lastBuildDate,description,ttl,link, wind, atmosphere, astronomy, image, item.link,item.lat,item.long,item.pubDate, item.forecast,item.language, item.title,item.condition,item.description,item.guid from weather.forecast where (woeid in (select woeid from geo.places(1) where text='" + city + "') and item.forecast.day='" + day + "') and u =' c' limit 1"
+
+
+def makeYqlQuery2(req,city):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    #city = parameters.get("geo-city")
+    if city is None:
+        return None
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "') and u= ' c'"
+
+def makeWebhookResult1(data,city):
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+        
+    if condition is None:
+        return {}
+
+    forecast=item.get('forecast')   
+    # print(json.dumps(item, indent=4))
+    
     
 
-def makeWebhookResult2(data):
-    # aqi=data.get('breezometer_aqi')
-    #print(json.dumps(data, indent=4))
-    print(data.get('breezometer_aqi'))
-    [item.encode('utf-8') for item in data]
-    print(data['random_recommendations'])
+    speech = "Weather in " + location.get('city') + " on "+ forecast.get('date')+ ": "+forecast.get('text')+", with a high of " + forecast.get('high') +" "+units.get('temperature')+ \
+             " and a low of " + forecast.get('low') +" "+ units.get('temperature')
 
-    recommendations=data['random_recommendations']
-    [item.encode('utf-8') for item in recommendations]
-    dom=data['dominant_pollutant_text']
-    [item.encode('utf-8') for item in dom]
-    print(dom)
-    print(data.get('breezometer_aqi'))
-    print(json.dumps(data, indent=4))
+    print("Response:")
+    print(speech)
 
-    speech = "abcd in "+ data['breezometer_aqi'] + " with Air quality index of "
+    slack_message = {
+        "text": speech,
+    }
 
+    print(json.dumps(slack_message))
 
+    return {
+        "speech": speech,
+        "displayText": speech,
+        "data": {"slack": slack_message},
+    }
 
+def pollevel(lati, longi):
+    lati=str(lati)
+    longi=str(longi)
+    aqi_url="https://api.breezometer.com/baqi/?lat="+lati+"&lon="+longi+"&key=e89840e7891c4a81a95e10156c1d9dcf&fields=breezometer_aqi"
+    aqi_req = requests.get(aqi_url)
+    aqi_json = json.loads(aqi_req.text)
+    aqi= aqi_json['breezometer_aqi']
+    return aqi
+def makeWebhookResult2(data,city):
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    lati=item.get('lat')
+    longi=item.get('long')
+    
+    aqi= pollevel(lati,longi)
+
+    print("aqi is")
+    print(aqi)
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
+    
+    
+    print(json.dumps(item, indent=4))
+
+    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
     print("Response:")
     print(speech)
@@ -107,7 +211,7 @@ def makeWebhookResult2(data):
         "displayText": speech,
         "data": {"slack": slack_message},
         # "contextOut": [],
-        "source": "webhook"
+        "source": "apiai-weather-webhook-sample"
     }
     
     
